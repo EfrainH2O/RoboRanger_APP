79b8a8c845c92c080354154188d9583fb0b1dca9
SerGG52151 <a01571538@tec.mx>
2025-10-08 23:09:39 -0600

    Colors Added

--- app/src/main/java/com/example/roboranger/ui/theme/Color.kt
+++ app/src/main/java/com/example/roboranger/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.example.roboranger.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryForestGreen = Color(0xFF4E7029)
+val SecondaryForestGreen = Color(0xFF679436)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryGrassGreen = Color(0xFFA5BE00)
+val SecondaryGrassGreen = Color(0xFF82BB44)
+
+val PrimaryBushGreen = Color(0xFF6A994E)
+val SecondaryBushGreen = Color(0xFF9BC86A)
+
+val PrimaryLightGreen = Color(0xFFCDE4B4)
+val SecondaryLightGreen = Color(0xFFD6E5CC)
+
+val PrimaryDarkGreen = Color(0xFF344B1B)
+
+val PrimaryRed = Color(0xFFBC4749)
+val PrimaryYellow = Color(0xFFBC9747)
+val PrimaryWhite = Color(0xFFFFFFFF)
+val PrimaryBlack = Color(0xFF1D1B20)
